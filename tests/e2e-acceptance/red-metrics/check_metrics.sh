e3256f8bd3c9966ede924fca6028c72fbf4cf0fa
Ishwar Kanse <ikanse@redhat.com>
2024-06-10 19:24:16 +0530

    Update acceptance tests

---
+++ tests/e2e-acceptance/red-metrics/check_metrics.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TOKEN=$(oc create token prometheus-user-workload -n openshift-user-workload-monitoring)
+THANOS_QUERIER_HOST=$(oc get route thanos-querier -n openshift-monitoring -o json | jq -r '.spec.host')
+
+#Check metrics used in the prometheus rules created for TempoStack. Refer issue https://issues.redhat.com/browse/TRACING-3399 for skipped metrics.
+metrics="duration_bucket duration_count duration_sum calls"
+
+for metric in $metrics; do
+query="$metric"
+count=0
+
+# Keep fetching and checking the metrics until metrics with value is present.
+while [[ $count -eq 0 ]]; do
+    response=$(curl -k -H "Authorization: Bearer $TOKEN" -H "Content-type: application/json" "https://$THANOS_QUERIER_HOST/api/v1/query?query=$query")
+    count=$(echo "$response" | jq -r '.data.result | length')
+
+    if [[ $count -eq 0 ]]; then
+    echo "No metric '$metric' with value present. Retrying..."
+    sleep 5  # Wait for 5 seconds before retrying
+    else
+    echo "Metric '$metric' with value is present."
+    fi
+  done
+done

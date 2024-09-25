396ee475680a86153575988c5a9b24b27b360bcc
skordas <skordas@redhat.com>
2024-09-25 12:26:00 -0400

    Switching to honeycombio trace generator.

--- tests/perfscale-sizing-recommendation/01-test.sh
+++ tests/perfscale-sizing-recommendation/01-test.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-rate=1000
+rate=40 # traces per second
+runtime=4200 # Time in seconds
+tracecount=$(($rate*$runtime))
 
 oc delete job generate-traces -n test-generate-traces
 
-cat ./content/05-generate-traces.yaml | sed "s/%RATE_NUMBER%/$rate/g" | oc create -f -
+cat ./content/05-generate-traces.yaml \
+| sed "s/%RATE_NUMBER%/$rate/g" \
+| sed "s/%RUN_TIME%/${runtime}s/g" \
+| sed "s/%TRACE_COUNT%/$tracecount/g" \
+| oc create -f -
